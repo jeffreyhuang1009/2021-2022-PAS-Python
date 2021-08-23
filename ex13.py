fd32ebb9330d1395e5c8145002462931fb324d66
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-23 19:18:54 +0800

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -1,7 +1,10 @@
+#imports commands
 from sys import argv
 
+#unpacked arguments
 script, first, second, third = argv
 
+#print stuff
 print("The script is called:",script)
 print("Your first variable is called:",first)
 print("Your second variable is called:",second)

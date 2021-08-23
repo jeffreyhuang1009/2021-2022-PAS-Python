01950ebaef35f8a48db5701326889d3a1c93d0ce
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-23 19:19:57 +0800

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,23 +1,38 @@
+#print the sentence
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+#print Hens and 30
+print("Hens", 25.0 + 30.0 / 6.0)
+#print Roosters and 97
+print("Roosters", 100.0 - 25.0 * 3.0% 4.0)
 
+#print sentence
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#print 6.75
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
+#print the sentence
 print("Is it true that 3 + 2 < 5 - 7?")
 
-print(3 + 2 < 5 - 7)
+#print false
+print(3.0 + 2.0 < 5.0 - 7.0)
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+#print sentence and 5
+print("What is 3 + 2?", 3.0 + 2.0)
+#print sentence and -2
+print("What is 5 - 7?", 5.0 - 7.0)
 
+#print sentence
 print("Oh, that's why it's False.")
-
+#print sentence
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+#print sentence and true
+print("Is it greater?", 5.0 > -2.0)
+#print sentence and true
+print("Is it greater or equal?", 5.0 >= -2.0)
+#print sentence and false
+print("Is it less or equal?", 5.0 <= -2.0)
+
+print(3*4*5*6*7.0)

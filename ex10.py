dadd221bd15deddeeb76b127c0d215dabbb8f435
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-23 19:18:41 +0800

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -2,14 +2,28 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat="I'm split\non a line."
 backslash_cat="I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* catnip\n\t*Grass
-"""
+'''
 
 print (tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+
+#know the escape sequences
+#\\ Backslash (\)
+#\' Single-quote (') \" Double-quote (")
+#\a ASCII bell (BEL)
+#\b ASCII backspace (BS)
+#\f ASCII formfeed (FF)
+#\n ASCII linefeed (LF)
+#\N{name} Character named name in the Unicode database (Unicode only) \r ASCII carriage return (CR)
+#\t ASCII horizontal tab (TAB)
+#\uxxxx Character with 16-bit hex value xxxx (Unicode only) \Uxxxxxxxx Character with 32-bit hex value xxxxxxxx (Unicode only) \v ASCII vertical tab (VT)
+#\ooo Character with octal value oo
+#\xhh Character with hex value hh

81a4acf5788ab72e01c8d5b5f6523bc0474f88fb
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-23 19:19:15 +0800

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -1,12 +1,21 @@
+#import stuff
 from sys import argv
 
+#unpacking stuff
 script, filename = argv
 
+#call function on txt
 txt = open(filename)
 
+#print filename
 print("Here's your file %r: "%(filename))
+#print what it reads in the file
 print(txt.read())
+#prints stuff
 print("type filename again:")
+#filename is what u input again
 file_again = input("> ")
+#txtagain calls an open function on the file
 txt_again = open(file_again)
+#print what it reads
 print(txt_again.read())

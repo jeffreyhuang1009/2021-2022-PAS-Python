92d792ddc5a6af7cc7496d32ed43a18647b15cc7
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-23 19:19:05 +0800

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -5,4 +5,16 @@ height = input()
 print("How much do you weigh?", end=" ")
 weight = input()
 
+
+
+#another form
+print("How big are you?", end=" ")
+age = input()
+print("How small are you?", end=" ")
+height = input()
+print("How short are you?", end=" ")
+weight = input()
+
 print("So, you're %r old, %r tall and %r heavy."%(age,height,weight))
+
+print('6\'2"')

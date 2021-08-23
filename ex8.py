86a88e830564419ffe1089ec0b974e9314eca579
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-23 19:20:09 +0800

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,10 +1,15 @@
 #a formatter where we can insert stuff
 formatter = "{} {} {} {}"
 
+#print stuff by filling in spaces, the stuff is after the parenthesis in .format
 print(formatter.format(1, 2, 3, 4))
+#print stuff by filling in spaces, the stuff is after the parenthesis in .format
 print(formatter.format("one", "two", "three", "four"))
+#print stuff by filling in spaces, the stuff is after the parenthesis in .format
 print(formatter.format(True, False, False, True))
+#print stuff by filling in spaces, the stuff is after the parenthesis in .format
 print(formatter.format(formatter, formatter, formatter, formatter))
+#print stuff by filling in spaces, the stuff is after the parenthesis in .format
 print(formatter.format(
     "Try your",
     "Own text here",

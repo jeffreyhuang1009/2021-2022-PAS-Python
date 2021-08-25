b75d743b2372cf6b985ca4e82ea286386c225768
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-08-25 09:29:47 +0800

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -2,10 +2,11 @@
 from sys import argv
 
 #unpacked arguments
-script, first, second, third = argv
+script, second, third = argv
 
 #print stuff
 print("The script is called:",script)
-print("Your first variable is called:",first)
 print("Your second variable is called:",second)
 print("Your third variable is called:",third)
+
+#deleted due to fewer argument on study drill

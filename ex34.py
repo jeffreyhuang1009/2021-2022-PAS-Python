b74e6e3cdaca16b573c57d7e750ae52596245601
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-09-04 15:56:43 +0800

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,16 @@
+'''
+1. The animal at 1 is the second animal and is a python
+2. The third animal is at 2 and is a peacock
+3. The first animal is at 0 and is a bear
+4. The animal at 3 is the fourth and is a kangaroo
+5. The fifth animal is at 4 and is a whale
+6. The animal at 2 is the third and is a peacock
+7. The sixth animal is at 5 and is a platypus
+8. The animal at 4 is fifth and is a whale
+'''
+
+#1. ordinal numerals or ordinal number words are words representing position or rank in a sequential order
+#1. cardinal numbers, or cardinals for short, are a generalization of the natural numbers used to measure the cardinality of sets
+#2. since the years are ordinal, meaning there are orders and in a sequence, so that's why it's like 1st 2nd 3rd rather than a counting including 0
+#3. ['a','b','c'], i made sure i can translate them
+#4. I checked

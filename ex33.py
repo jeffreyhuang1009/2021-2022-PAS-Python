3053294761e84b330fda54588211fec53e6dfbbe
jeffreyhuang1009 <jeffreyhuang1009@gmail.com>
2021-09-04 15:56:37 +0800

    Update ex33.py

--- ex33.py
+++ ex33.py
@@ -1,19 +1,20 @@
+#def loop(var, increment):
 def loop(var):
-    
-
-i = 0
-numbers = []
-
-while i < 6:
-    print ("At the top i is %d" % i)
-    numbers.append(i)
-
-    i = i + 1
-    print ("Numbers now: ", numbers)
-    print ("At the bottom i is %d" % i)
+    i = 0
+    numbers = []
+    for i in range(var):
+        print ("At the top i is %d" % i)
+        numbers.append(i)
+        #i+=increment
+        print ("Numbers now: ", numbers)
+        print ("At the bottom i is %d" % i)
 
 
-print ("The numbers: ")
+    print ("The numbers: ")
 
-for num in numbers:
-    print (num)
+    for num in numbers:
+        print (num)
+
+#loop(69,2) 2 was the increment
+loop(69)
+#dont need to do increments, so i deleted the increment variable
